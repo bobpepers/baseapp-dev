4f71f47ee5bc2fa41f06be72e693669b45756bf1
bobpepers <bob.pepers@gmail.com>
2020-03-28 16:30:51 +0100

    Fix: improve wallet icons

--- src/containers/Wallets/FailIcon.tsx
+++ src/containers/Wallets/FailIcon.tsx
@@ -4,10 +4,12 @@ export const FailIcon = () => {
     return (
         <Fragment>
             Failed
-            <svg width="10" height="10" viewBox="0 0 10 10" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path d="M1.00015 1L8.99993 9.00002" stroke="#991A24" strokeWidth="2" strokeLinecap="round"/>
-                <path d="M8.99988 1L1.0001 9.00002" stroke="#991A24" strokeWidth="2" strokeLinecap="round"/>
-            </svg>
+            <div className="ml-5">
+                <svg width="15" height="15" viewBox="0 0 10 10" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M1.00015 1L8.99993 9.00002" stroke="#991A24" strokeWidth="2" strokeLinecap="round"/>
+                    <path d="M8.99988 1L1.0001 9.00002" stroke="#991A24" strokeWidth="2" strokeLinecap="round"/>
+                </svg>
+            </div>
         </Fragment>
     );
 };
--- src/containers/Wallets/SucceedIcon.tsx
+++ src/containers/Wallets/SucceedIcon.tsx
@@ -5,15 +5,17 @@ export const SucceedIcon = () => {
     return (
         <Fragment>
             Success
-            <svg width="10" height="8" viewBox="0 0 10 8" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path
-                    d="M9 1L3.68517 7L1 4.47371"
-                    stroke="#14560F"
-                    strokeWidth="2"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                />
-            </svg>
+            <div className="ml-5">
+                <svg width="15" height="15" viewBox="0 0 10 8" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path
+                        d="M9 1L3.68517 7L1 4.47371"
+                        stroke="#14560F"
+                        strokeWidth="2"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                    />
+                </svg>
+            </div>
         </Fragment>
     );
 };
