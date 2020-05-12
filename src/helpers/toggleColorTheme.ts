97e9612f03d5dc5b05dab723e3e29a42af93bb58
bobpepers <bob.pepers@gmail.com>
2020-05-12 22:20:13 +0200

    update

--- src/helpers/toggleColorTheme.ts
+++ src/helpers/toggleColorTheme.ts
@@ -1,7 +1,7 @@
 export const toggleColorTheme = (value: string) => {
     const rootElement = document.getElementsByTagName('body')[0];
     if (value === 'light') {
-    	rootElement && rootElement.classList.remove('dark-theme');
+        rootElement && rootElement.classList.remove('dark-theme');
         rootElement && rootElement.classList.add('light-theme');
     } else {
         rootElement && rootElement.classList.remove('light-theme');
