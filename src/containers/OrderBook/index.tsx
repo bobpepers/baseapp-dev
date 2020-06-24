30ef85edf75069b7f89133e70f60c64a5c81e063
bobpepers <bob.pepers@gmail.com>
2020-06-24 21:51:19 +0200

    update

--- src/containers/OrderBook/index.tsx
+++ src/containers/OrderBook/index.tsx
@@ -192,33 +192,33 @@ class OrderBookContainer extends React.Component<Props, State> {
                        })()
                     }
                 </Grid>
-                <Grid item xs={12} style={{'height': '44%'}} className="orderbook-text-color">
+                <Grid item xs={12} style={{height: '44%'}} className="orderbook-text-color">
                     {arrayBids.map((object, i) => {
                         const currentPercentage = Math.floor((object[4] / totalBids) * 100);
                         if (object[0] === "empty") {
                             return (
-                                <Grid container key={i} className="orderbook-border-bottom" style={{'position': 'relative'}}>
-                                    <Grid container item xs={12} justify="center" style={{'zIndex': 30}}>
+                                <Grid container key={i} className="orderbook-border-bottom" style={{position: 'relative'}}>
+                                    <Grid container item xs={12} justify="center" style={{zIndex: 30}}>
                                         {object[1]}
                                     </Grid>
                                 </Grid>
                             );
                         } else {
                             return (
-                                <Grid container key={i} onClick={e => this.handleOnSelectBids(e, i)} className="orderbook-border-bottom orderbook-item" style={{'position': 'relative', 'cursor': 'pointer'}}>
-                                    <Grid container item xs={3} justify="center" style={{'zIndex': 30}}>
+                                <Grid container key={i} onClick={e => this.handleOnSelectBids(e, i)} className="orderbook-border-bottom orderbook-item" style={{position: 'relative', cursor: 'pointer'}}>
+                                    <Grid container item xs={3} justify="center" style={{zIndex: 30}}>
                                         {object[0]}
                                     </Grid>
-                                    <Grid container item xs={3} justify="center" style={{'zIndex': 30}}>
+                                    <Grid container item xs={3} justify="center" style={{zIndex: 30}}>
                                         {object[1]}
                                     </Grid>
-                                    <Grid container item xs={3} justify="center" style={{'zIndex': 30}}>
+                                    <Grid container item xs={3} justify="center" style={{zIndex: 30}}>
                                         {object[2]}
                                     </Grid>
-                                    <Grid container item xs={3} justify="center" style={{'zIndex': 30}}>
+                                    <Grid container item xs={3} justify="center" style={{zIndex: 30}}>
                                         {object[3]}
                                     </Grid>
-                                    <span style={{'backgroundColor': 'rgba(0, 169, 44, 0.2)', 'position': 'absolute', 'right': 0, 'top': 0, 'bottom': 0, 'zIndex': 20, 'width': `${currentPercentage}%`}}/>
+                                    <span style={{backgroundColor: 'rgba(0, 169, 44, 0.2)', position: 'absolute', right: 0, top: 0, bottom: 0, zIndex: 20, width: `${currentPercentage}%`}}/>
                                 </Grid>
                             );
                         }
